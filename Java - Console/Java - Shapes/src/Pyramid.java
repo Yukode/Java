d792fc52eff32eca8a5156def3ee3e11f861b309
Yuki says hi <SnowyyukiiiiiYukode@gmail.com>
2023-01-14 18:33:07 +0800

    Enjoy!

---
+++ Java - Console/Java - Shapes/src/Pyramid.java
@@ -0,0 +1,52 @@
+// Under construction :>. I am not good at this patterns.
+
+import java.util.Scanner;
+
+public class Pyramid {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter Size: ");
+        int size = scanner.nextInt();
+        
+        System.out.println();
+        for(int a = 0; a < size; a++) {
+            for(int b = 0; b < size; b++) {
+                
+                if(a >= b) {
+                    System.out.print(b + " ");
+                }
+                
+            }
+            System.out.println();
+        }
+        
+        System.out.println();
+        for(int a = size; a >= 0; a--) {
+            for(int b = size; b >= 0; b--) {
+                
+                if(a > b) {
+                    System.out.print(b + " ");
+                }
+                
+            }
+            System.out.println();
+        }
+        
+    }
+}
+
+/*
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+
+        5
+      4 5 4
+    3 4 5 4 3
+  2 3 4 5 4 3 2
+1 2 3 4 5 4 3 2 1
+*/
