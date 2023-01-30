a36de01e1fcfc2e78677a62ffa08ebb0481dfbdf
Yuki says hi <SnowyyukiiiiiYukode@gmail.com>
2023-01-30 20:48:49 +0800

    Enjoy!

---
+++ Java - Code Chef Problems/00README.txt
@@ -0,0 +1,1 @@
+NumberMirror - START01
---
+++ Java - Code Chef Problems/NumberMirror.java
@@ -0,0 +1,26 @@
+/*
+Problem
+Write a program that takes a number N as the input, and prints it to the output.
+
+Input Format
+The only line contains a single integer.
+
+Output Format
+Output the answer in a single line.
+
+Input: 123
+Output: 123
+*/
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner s = new Scanner(System.in);
+	    System.out.println(s.nextInt());
+	}
+}
